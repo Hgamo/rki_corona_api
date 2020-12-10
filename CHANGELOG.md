59e8e3ceb1e39e48dc9f27c970b2e57e637664fd
Rounded Infinity <rubengullatz@gmail.com>
2020-12-10 17:06:58 +0100

    1.0.2 released

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 1.0.0
 
 - Initial version, created by Stagehand
+
+## 1.0.1
+
+- Fixed export
+- Longer description for more pub points
--- lib/rki_corona_api.dart
+++ lib/rki_corona_api.dart
@@ -1,8 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library rki_corona_api;
 
 export 'src/rki_corona_api_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/data/cases.dart';
+export 'src/data/districts.dart';
+export 'src/data/states.dart';
